25fbed067f6692ce51e5b6a87a54be56b3291b71
KABANYANA <joyeusekabanyana@gmail.com>
2026-03-03 15:13:23 +0200

    feat(backend): configure Book model with fillable attributes and casting

--- backend/app/Models/Book.php
+++ backend/app/Models/Book.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'author',
+        'isbn',
+        'publication_year',
+        'is_available',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_available' => 'boolean',
+        'publication_year' => 'integer',
+    ];
 }
