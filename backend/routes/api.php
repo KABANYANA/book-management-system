ae6ba4d8deddd3e177ea317621e5b911996e8334
KABANYANA <joyeusekabanyana@gmail.com>
2026-03-03 15:29:14 +0200

    refactor(backend): use apiResource for cleaner REST routing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,9 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
-Route::prefix('books')->group(function () {
-    Route::get('/', [BookController::class, 'index']);
-    Route::post('/', [BookController::class, 'store']);
-    Route::put('/{book}', [BookController::class, 'update']);
-    Route::delete('/{book}', [BookController::class, 'destroy']);
-});
+Route::apiResource('books', BookController::class);
